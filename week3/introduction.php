c7f809a695894c794bea2d2572968853c6dd5ffb
eyyy0704 <125983426+eyyy0704@users.noreply.github.com>
2024-03-17 20:32:24 +0800

    Add files via upload

---
+++ week3/introduction.php
@@ -0,0 +1,78 @@
+<html>
+
+<head>
+    <meta charset = uf8>
+    <meta name = 介紹>
+    <title>介紹</title>
+</head>
+
+<body>
+    <body bgcolor = #85929E>
+    <center>
+    <img src="/homework/HW-week3/img/2.png" width = 400 height = 200>
+    <style>
+        .transparent-box {
+            background-color: rgba(255, 255, 255, 0.5);
+            width: 450px; 
+            height: 250px;
+            padding: 0px; 
+        }
+    </style>
+    <div class="transparent-box">
+        <div style="text-align: center; margin: 0 auto; width: 420px;">
+            <div style="line-height: 35px; text-align: left;"><b>
+                <ul>
+                    <br/>
+                    <li>原文名 : 스트레이 키즈
+                    <li>英文名 : Stray Kids
+                    <li>國籍 : 南韓
+                    <li>出道作品 : 南韓《I am NOT》日本《SKZ2020》
+                    <li>出道日期 : 南韓 2018年3月25日 日本 2020年3月18日
+                    <li>youtube 頻道連結 : <a href = "https://www.youtube.com/@StrayKids" target = "new">點進去</a>
+                </ul>
+            </div></b>
+        </div>          
+    </div>
+    <br/><b><font size = "5" color = "black">成員介紹</font></b><br/><br/>
+        <style>
+            .container {
+                display: flex;
+                align-items: center;
+            }
+            table {
+                border-collapse: collapse;
+                margin-left: 350px;
+                margin-right: 20px;
+                font-size: 15px;
+                border: 1px solid black;
+                width: 35%;
+                height: 25%;
+            }
+            td, th {
+                border: 1px solid black;
+                padding: 8px;
+                text-align: center;
+                border: 1px solid black;
+                background-color: white;
+            }
+            th {
+                background-color: gray;
+            }
+        </style>
+        <div class="container">
+            <table>
+                <tr><th>藝名</th><th>生日</th><th>定位</th></tr>
+                <tr><td>方燦</td><td>1997/10/3</td><td>隊長、製作、vocal、rap、dance</td></tr>
+                <tr><td>Lee Know</td><td>1998/10/25</td><td>dance、vocal</td></tr>
+                <tr><td>彰彬</td><td>1999/8/11</td><td>rap、製作</td></tr>
+                <tr><td>鉉辰</td><td>2000/3/20</td><td>dance、rap、門面</td></tr>
+                <tr><td>Han</td><td>2000/9/14</td><td>rap、製作、vocal</td></tr>
+                <tr><td>Felix</td><td>2000/9/15</td><td>dance、rap</td></tr>
+                <tr><td>昇玟</td><td>2000/9/22</td><td>vocal</td></tr>
+                <tr><td>I.n</td><td>2001/2/8</td><td>vocal、忙內</td></tr>
+            </table>
+            <img src="/homework/HW-week3/img/1.jpg" width = 200 height = 320>
+        </div>
+</body>
+
+</html>
